63e24dde94d5acc3468734466dd0aa5cbe73b6a6
danielhallmann <daniel.hallmann@me.com>
2024-01-16 23:32:32 +0100

    Uncomment debuging and fix gitignore

--- app/manage.py
+++ app/manage.py
@@ -11,11 +11,11 @@ cli = FlaskGroup(create_app=create_app)
 
 @cli.command('estimate_total_cost')
 def estimate_total_cost():
-  #"""
+  """
   debugpy.listen(("0.0.0.0", 5678))
   print("Waiting for client to attach...")
   debugpy.wait_for_client()
-  #"""
+  """
   ot.estimate_total_cost()
 
 if __name__ == "__main__":
